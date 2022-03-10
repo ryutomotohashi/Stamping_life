1e5caa0aca59415b3626e16d2d424c8641688d37
ryutomotohashi <ry_u333@icloud.com>
2022-03-10 17:24:05 +0000

    [add]devise関連

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,9 +2,9 @@ const { environment } = require('@rails/webpacker')
 
 module.exports = environment
 
-const webpack = require('wabpack')
+const webpack = require('webpack')
 environment.plugins.prepend(
- 'Provide',
+  'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([{name: "荒川くん", introduction: "荒川です"}, {name: "足立くん",introduction: "足立です"}, {name: "北くん",introduction: "北です"}])
