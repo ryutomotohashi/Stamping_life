6e8098730f6a3b8e28cd07c3e29ab35d8d971f77
ryutomotohashi <ry_u333@icloud.com>
2022-03-13 04:59:46 +0000

    投稿機能完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,36 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  #自分がフォローする側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+  #自分がフォローした人の一覧をフォローされた人(followed)を参照して出力
+  has_many :followings, through: :relationships, source: :followed
+
+  #自分がフォローされる側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
+  #自分をフォローしている人の一覧をフォローした人(follower)を参照して出力
+  has_many :followers, through: :reverse_of_relationships,source: :follower
+
+  has_many :targets, dependent: :destroy
+
   has_one_attached :profile_image
 
+  #画像を取得する処理
   def get_icon
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+  #フォローする際の処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  #フォローを外す際の処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  #フォローしているかの判別
+  def following?(user)
+    followings.include?(user)
   end
 end
